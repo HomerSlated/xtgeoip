c091dc1e30580d44188a62807b3442debc33ced6
Homer <github@slated.org>
2026-03-31 20:29:43 +0000

    Update

--- src/messages.rs
+++ src/messages.rs
@@ -9,14 +9,17 @@ pub fn log_print(msg: &str, level: Level) {
     log::log!(level, "{msg}");
 }
 
+#[allow(dead_code)]
 pub fn info(msg: &str) {
     log_print(msg, Level::Info);
 }
 
+#[allow(dead_code)]
 pub fn warn(msg: &str) {
     log_print(msg, Level::Warn);
 }
 
+#[allow(dead_code)]
 pub fn error(msg: &str) {
     log_print(msg, Level::Error);
 }
