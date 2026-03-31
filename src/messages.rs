b6e9c611305b2f27ff06c3ba68a5da20b58a0551
Homer <github@slated.org>
2026-03-31 20:22:11 +0000

    Update

--- src/messages.rs
+++ src/messages.rs
@@ -1,21 +1,22 @@
 use log::Level;
 
-/// Log a message at the given level
 pub fn log_print(msg: &str, level: Level) {
+    match level {
+        Level::Error | Level::Warn => eprintln!("{msg}"),
+        _ => println!("{msg}"),
+    }
+
     log::log!(level, "{msg}");
 }
 
-/// Log an info message
 pub fn info(msg: &str) {
     log_print(msg, Level::Info);
 }
 
-/// Log a warning message
 pub fn warn(msg: &str) {
     log_print(msg, Level::Warn);
 }
 
-/// Log an error message
 pub fn error(msg: &str) {
     log_print(msg, Level::Error);
 }
