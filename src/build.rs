acdfe6afc4b889e24a16ffaf370622afa7b47eb9
Homer <github@hazensparkle.co.uk>
2026-04-01 20:15:53 +0000

    Update

--- src/build.rs
+++ src/build.rs
@@ -12,7 +12,7 @@ use ipnetwork::IpNetwork;
 use rayon::prelude::*;
 use sha2::{Digest, Sha256};
 
-use crate::messages::{info, warn},
+use crate::messages::{info, warn};
 
 /// Country data with IPv4 and IPv6 ranges
 #[derive(Default)]
