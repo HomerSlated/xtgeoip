c6fb15762ac8e2c25853ac3f88ae9040a05bda11
Homer <github@slated.org>
2026-04-01 16:40:54 +0000

    Update

---
+++ src/bin/logger.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use chrono::Local;
+use log::info;
+
+fn setup_logger() -> Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} [{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Info)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("/var/log/testlog.log")?)
+        .apply()?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    setup_logger()?;
+    info!("Hello, World!");
+    Ok(())
+}
